fee1b092ff2f6f7d2f61e54bca47feb589edfc28
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-23 22:14:25 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@
 let consoleStr = '';
 const mainElem = document.getElementById("categories");
 consoleStr += `Number of categories: ${mainElem.children.length}`;
-for(const elem of mainElem.children){
+mainElem.children.forEach(elem => {
     consoleStr += "\n\n";
     consoleStr += `Category: ${elem.children[0].innerText}\n`
     + `Elements: ${elem.children[1].children.length}`;
